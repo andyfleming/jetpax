d56a19604b067a5f78e29fa119957c077a5b2d37
Andy Fleming <andy@andyfleming.com>
2019-01-29 22:26:54 -0800

    Adding UI hosting to server

--- src/Server/server.ts
+++ src/Server/server.ts
@@ -1,9 +1,24 @@
 import express from 'express'
+import path from 'path'
 
 const server = express()
 
+// Serve static files
+server.use(express.static(path.join(process.cwd(), 'ui/build')))
+
+
+// API Routes
+// ------------------------------------------------------------------------------------
+
 server.get('/api/online', (req, res) => {
     res.send('Jetpax Server API Online')
+})
+
+// ------------------------------------------------------------------------------------
+
+// Fallback for rewrite to index.html
+server.get('*', (req,res) =>{
+    res.sendFile(path.join(process.cwd(), 'ui/build/index.html'))
 })
 
 export default server
