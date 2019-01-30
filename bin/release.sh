fc88b95021bcec462307c4bdfc8f7a81dcb436e4
Andy Fleming <andy@andyfleming.com>
2019-01-30 15:38:24 -0800

    Working around CRA preflight issue
    (similar to similar to
    https://stackoverflow.com/questions/53089122/issue-with-babel-jest-depen
    dency-when-running-npm-start-in-a-react-app)

--- bin/release.sh
+++ bin/release.sh
@@ -12,8 +12,8 @@ npm run build
 
 # Test and build react project
 cd ui
-CI=true SKIP_PREFLIGHT_CHECK=true npm test  -- --ci # similar to https://stackoverflow.com/questions/53089122/issue-with-babel-jest-dependency-when-running-npm-start-in-a-react-app
-npm run build
+npm run test:ci
+npm run build:ci
 
 cd ..
 
