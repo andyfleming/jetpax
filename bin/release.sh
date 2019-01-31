abddfe6ada24045722ce10cf54b2c6d3acb1b785
Andy Fleming <andy@andyfleming.com>
2019-01-30 16:04:19 -0800

    Adding prettier output to release script

--- bin/release.sh
+++ bin/release.sh
@@ -2,20 +2,43 @@
 
 set -euo pipefail
 
-# Clean up build artifacts
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NO_COLOR='\033[0m' # No Color
+
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+echo -e "${BLUE} Removing existing build artifacts... ${NO_COLOR}"
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+
 rm -rf build
 rm -rf ui/build
 
-# Test and build root project
+echo ""
+echo -e "${GREEN} Removed. ${NO_COLOR}"
+
+echo ""
+
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+echo -e "${BLUE} Testing and building root project... ${NO_COLOR}"
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+
+
 npm test
 npm run build
 
-# Test and build react project
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+echo -e "${BLUE} Testing and building UI ${NO_COLOR}"
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+
 cd ui
 npm run test:ci
 npm run build:ci
 
 cd ..
 
-# Run np (publish workflow tool)
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+echo -e "${BLUE} Running np for publishing... ${NO_COLOR}"
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+
 ./node_modules/.bin/np
