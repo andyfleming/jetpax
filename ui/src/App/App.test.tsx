d24f0b424b1e5ff655aa6feec35caad8a417f9af
Andy Fleming <andy@andyfleming.com>
2019-02-06 16:54:56 -0800

    Updating test to have required providers

--- ui/src/App/App.test.tsx
+++ ui/src/App/App.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import makeStore from "./Store/makeStore"
+import getDefaultDependencies from "./Dependencies/getDefaultDependencies"
+import {Provider} from "react-redux"
+import {MemoryRouter} from "react-router"
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  const deps = getDefaultDependencies()
+  const store = makeStore(deps)
+  ReactDOM.render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      </Provider>, div)
   ReactDOM.unmountComponentAtNode(div)
 })
