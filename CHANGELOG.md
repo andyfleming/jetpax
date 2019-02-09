bd22dfffa285328688688f26a3c10e2a67202c78
Andy Fleming <andy@andyfleming.com>
2019-02-09 00:15:50 -0800

    Fixing missed updates from router state changes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Project internals
     - CSS -> SCSS
 
+### Fixed
+- Nav links delayed update issue
+
 ## [0.4.0] - 2019-02-06
 ### Added
 - UI Scaffolding
--- ui/src/NavBar.tsx
+++ ui/src/NavBar.tsx
@@ -9,7 +9,17 @@ import {
 } from "@blueprintjs/core"
 import {connect} from "react-redux"
 import {RootState} from "./App/Store/makeStore"
-import {Link} from "react-router-dom"
+import {NavLink} from "react-router-dom"
+
+type NavBarLinkProps = {
+    path: string
+    icon: IconName
+    text: string
+}
+
+const NavBarLink = ({path, icon, text}: NavBarLinkProps) => (
+    <NavLink to={path} activeClassName="selected"><Button className={Classes.MINIMAL} icon={icon} text={text} /></NavLink>
+)
 
 type Props = {
     request: boolean
@@ -17,27 +27,19 @@ type Props = {
     event: boolean
 }
 
-function at(path: string) {
-    return (window.location.pathname === path)
-}
-
-const NavLink = ({path, icon, text}: {path: string, icon: IconName, text: string}) => (
-    <Link to={path}><Button className={`${Classes.MINIMAL} ${at(path) && 'selected'}`} icon={icon} text={text} /></Link>
-)
-
 const NavBar = ({request, reply, event}: Props) => {
     return (
         <Navbar className={Classes.DARK}>
             <NavbarGroup align="left">
                 <NavbarHeading className="logo"/>
-                <NavLink path="/dashboard" icon="home" text="Dashboard" />
-                <NavLink path="/services" icon="applications" text="Services" />
-                <NavLink path="/assets" icon="compressed" text="Assets" />
-                <NavLink path="/configuration" icon="code-block" text="Configuration" />
-                <NavLink path="/docs" icon="list-detail-view" text="Docs" />
+                <NavBarLink path="/dashboard" icon="home" text="Dashboard" />
+                <NavBarLink path="/services" icon="applications" text="Services" />
+                <NavBarLink path="/assets" icon="compressed" text="Assets" />
+                <NavBarLink path="/configuration" icon="code-block" text="Configuration" />
+                <NavBarLink path="/docs" icon="list-detail-view" text="Docs" />
             </NavbarGroup>
             <NavbarGroup align="right">
-                <NavLink path="/settings" icon="settings" text="Settings" />
+                <NavBarLink path="/settings" icon="settings" text="Settings" />
                 <Button className={`${Classes.MINIMAL} ${request && 'selected'}`} icon="cloud-upload" />
                 <Button className={`${Classes.MINIMAL} ${reply && 'selected'}`} icon="cloud-download" />
                 <Button className={`${Classes.MINIMAL} ${event && 'selected'}`} icon="satellite" />
@@ -52,4 +54,4 @@ export default connect((state: RootState) => {
         reply: state.webSocketActivity.reply,
         event: state.webSocketActivity.event,
     }
-})(NavBar)
+}, null, null, { pure: false})(NavBar)
