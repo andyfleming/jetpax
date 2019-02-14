e17ff79d0ab515c243bae6d4dbec4b06c985ab80
Andy Fleming <andy@andyfleming.com>
2019-02-13 18:34:44 -0800

    Converting unhandled promises into thrown errors

--- src/Server/run-server.ts
+++ src/Server/run-server.ts
@@ -3,6 +3,10 @@ import makeServer from './makeServer'
 import getDependencies from "./Dependencies/getDependencies"
 import serverPort from "../Common/serverPort"
 
+process.on('unhandledRejection', (err) => {
+    throw err
+})
+
 async function runServer() {
     const deps = await getDependencies()
     const server = await makeServer(deps)
