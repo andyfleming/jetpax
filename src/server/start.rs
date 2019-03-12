c7b7d9c0368a809bacd9e3077eb095e77fc03e48
Andy Fleming <andy@andyfleming.com>
2019-03-11 23:04:06 -0700

    WIP: Testing server state

--- src/server/start.rs
+++ src/server/start.rs
@@ -2,6 +2,40 @@ use std::process;
 use daemonize::Daemonize;
 use super::super::server::handlers;
 use rocket_contrib::serve::StaticFiles;
+use rocket::State;
+use std::sync::{Mutex, Arc, RwLock};
+use core::borrow::Borrow;
+
+#[derive(Debug)]
+struct SystemState {
+    count: RwLock<u32>
+}
+
+impl SystemState {
+    fn new() -> SystemState {
+        SystemState {
+            count: RwLock::new(0)
+        }
+    }
+
+    fn increment(&mut self) {
+        let mut count = self.count.write().unwrap();
+        *count += 1;
+    }
+}
+
+#[get("/api/count")]
+fn get_count(state: State<SystemState>) -> String {
+    format!("Current count: {}", state.count.read().unwrap())
+}
+
+#[get("/api/inc")]
+fn increment_count(state: State<SystemState>) -> String {
+    let mut count = state.count.write().unwrap();
+    *count += 1;
+    format!("Incremented Count")
+}
+
 
 pub fn start(run_in_background: bool) {
     // TODO: handle startup concerns like ~/.jetpax directory existence
@@ -32,6 +66,8 @@ pub fn start(run_in_background: bool) {
         println!("Starting server (in foreground)...");
     }
 
+    let state = SystemState::new();
+
     rocket::ignite()
         .mount("/", StaticFiles::from("ui/build"))
         .mount("/", routes![
@@ -39,7 +75,10 @@ pub fn start(run_in_background: bool) {
             handlers::pid::handle,
             handlers::ui::index,
             handlers::ui::assets,
+            get_count,
+            increment_count,
         ])
+        .manage(state)
         .register(catchers![handlers::ui::rewrite_to_index])
         .launch();
 }
