31d4a51dc35caf731833553f1d212cfa744887fa
Andy Fleming <andy@andyfleming.com>
2019-03-12 14:08:47 -0700

    Adding titles for tasks in build-all

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NO_COLOR='\033[0m' # No Color
+
 echo "Note: This should be run from the root directory of the project"
 # TODO: consider enforcing programmatically
 
@@ -10,28 +15,54 @@ if [[ "$(uname)" != "Darwin" ]]; then
     exit 1;
 fi;
 
-# UI Tasks...
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+echo -e "${BLUE} Removing existing UI build artifacts... ${NO_COLOR}"
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+
 cd ui
 
-# Remove existing build
 rm -rf build
 
 # Install UI dependencies from scratch
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+echo -e "${BLUE} Installing UI dependencies (from scratch)... ${NO_COLOR}"
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+
 npm ci
 
 # Test UI code
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+echo -e "${BLUE} Testing UI... ${NO_COLOR}"
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+
 npm run test:ci
 
 # Build UI code (for "production")
-npm run build
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+echo -e "${BLUE} Building UI... ${NO_COLOR}"
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
 
-# Test rust (CLI/Server) project
+npm run build:ci
+
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+echo -e "${BLUE} Testing root project (CLI/Server)... ${NO_COLOR}"
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+
 cd ..
 cargo test
 
 # Build mac binary
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+echo -e "${BLUE} Building mac binary... ${NO_COLOR}"
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+
 
 cargo build --release
+
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+echo -e "${BLUE} Building linux binary (in Docker)... ${NO_COLOR}"
+echo -e "${BLUE}-----------------------------------------------------------------------------${NO_COLOR}"
+
 
 # Build linux binary
 ./bin/build-linux.sh
